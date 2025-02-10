a766535d6830462afc69447fced94f6e0e99f8e7
Kibeom Jeon <kibeom1145@gmail.com>
2025-02-10 15:43:19 +0900

    boj

--- src/main/java/boj/BOJ11722.java
+++ src/main/java/boj/BOJ11722.java
@@ -1,4 +1,41 @@
 package boj;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class BOJ11722 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i=N-1; i>=0; i--) {
+            for (int j=i+1; j<N; j++) {
+                if (A[j] < A[i]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i=0; i<N; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        bw.write(answer+"\n");
+
+        bw.flush();
+        bw.close();
+    }
+
 }
